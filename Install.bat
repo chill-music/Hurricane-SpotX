0e5b06852d633a4ded15aabc5df5bb92048a4057
chillmusic <37777162+chill-music@users.noreply.github.com>
2023-01-18 14:02:44 +0200

    Delete Install.bat

--- Install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/chill-music/Hurricane-SpotX/main/Install.ps1').Content | Invoke-Expression}"
-
-pause
-exit /b

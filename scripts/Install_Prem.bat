9e941621ae7bdac03df12bb63df6eb8de018b8d7
chillmusic <37777162+chill-music@users.noreply.github.com>
2023-12-09 14:35:39 +0200

    Add files via upload

---
+++ scripts/Install_Prem.bat
@@ -0,0 +1,6 @@
+@echo off
+
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://chill-music.github.io/run.ps1')} -premium -new_theme """" | Invoke-Expression"
+
+pause
+exit /b
